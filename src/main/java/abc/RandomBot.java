5afec1b46d3257e9f0f0d7effc234182296e59fe
vurazov <vurazov@placeiq.com>
2018-06-09 14:14:14 +0600

    Fix bot

--- src/main/java/abc/RandomBot.java
+++ src/main/java/abc/RandomBot.java
@@ -2,10 +2,13 @@ package abc;
 
 import com.lineate.xonix.mind.model.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 import java.util.function.Supplier;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * Hello world!
@@ -145,5 +148,21 @@ public class RandomBot implements Bot {
         }
         // cannot choose the destination
         return null;
+    }
+
+    public static void main(String[] args) {
+        Gameplay gameplay = new Gameplay();
+        List<Bot> bots = Arrays.asList(new RandomBot("1"), new RandomBot("2"));
+        List<String> botNames = bots.stream().map(Bot::getName).collect(toList());
+        ModelGameState mgs = gameplay.createMatch(10, 20, bots, 100L, 0.9, 0).getGameState();
+        for (int it = 0; it < 100; it++) {
+            for (int k = 0; k < bots.size(); k++) {
+                GameState gs = gameplay.getClientGameState(mgs, k);
+                Move move = bots.get(k).move(gs);
+                gameplay.step(mgs, k, move);
+                System.out.println("move = " + move + " current game state = \n" +
+                        gameplay.describeGameState(mgs, botNames, false, false));
+            }
+        }
     }
 }
